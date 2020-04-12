8aa339f29ad3a697c08ed2cab9467aaffcac979b
Tam Ton <tam.ton@apps-cyclone.com>
2020-04-12 10:25:20 +0700

    1. Create database 2. Create table 3. Insert records to table

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -4,11 +4,53 @@ var con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '12345678',
-    database: 'test',
+    database: 'nodejs_demo',
     port: 3306
 });
 
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
+    //createDatabase();
+    //createTable();
+    insertRecord();
 });
+
+function createDatabase() {
+    con.query("CREATE DATABASE nodejs_demo", function (err, result) {
+        if (err) throw err;
+        console.log("Database created!");
+    })
+}
+
+function createTable() {
+    var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created!");
+    });
+}
+
+function insertRecord(){
+    var sql = "INSERT INTO customers (name, address) VALUES ?";
+    var values = [
+        ['John', 'Highway 71'],
+        ['Peter', 'Lowstreet 4'],
+        ['Amy', 'Apple st 652'],
+        ['Hannah', 'Mountain 21'],
+        ['Michael', 'Valley 345'],
+        ['Sandy', 'Ocean blvd 2'],
+        ['Betty', 'Green Grass 1'],
+        ['Richard', 'Sky st 331'],
+        ['Susan', 'One way 98'],
+        ['Vicky', 'Yellow Garden 2'],
+        ['Ben', 'Park Lane 38'],
+        ['William', 'Central st 954'],
+        ['Chuck', 'Main Road 989'],
+        ['Viola', 'Sideway 1633']
+      ];
+    con.query(sql, [values], function (err, result) {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+    });
+}
